50389cb8a868a908cd1c65a769b97883f23d4a1a
Web3Prof <141943385+Web3Prof@users.noreply.github.com>
2024-10-20 03:18:32 +0700

    Update estimation scriipt

--- scripts/quoteTest.js
+++ scripts/quoteTest.js
@@ -5,81 +5,85 @@ const {
   abi: QUOTER_ABI,
 } = require('@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json');
 
+const { abi: SWAPV3_ABI } = require('../artifacts/contracts/SwapV3.sol/SwapV3.json');
+
 const QUOTER_ADDRESS = '0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a'; // Uniswap V3 Quoter contract address
 const WETH9_ADDRESS = '0x4200000000000000000000000000000000000006'; // WETH9 address
 const CBETH_ADDRESS = '0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22'; // cbETH address
+const SWAPV3_ADDRESS = '0xb03A1229B8B71cD5C97Abd10BE0238700970a770';
 const FEE_TIER = '3000'; // Pool fee
 
-async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(
-    process.env.BASE_MAINNET_URL
-  );
+const provider = new ethers.JsonRpcProvider(
+  process.env.BASE_MAINNET_URL
+);
 
-  // Create an instance of the Quoter contract
-  const quoterContract = new ethers.Contract(
-    QUOTER_ADDRESS,
-    QUOTER_ABI,
-    provider
-  );
+const quoterContract = new ethers.Contract(
+  QUOTER_ADDRESS,
+  QUOTER_ABI,
+  provider
+);
 
-  // Parameters for WETH -> cbETH swap
-  const wethToCbethParams = {
+const swapV3Contract = new ethers.Contract(SWAPV3_ADDRESS, SWAPV3_ABI, provider);
+
+async function getQuoteETHToCbETHExactIn(amountIn) {
+  const params = {
     tokenIn: WETH9_ADDRESS,
     tokenOut: CBETH_ADDRESS,
     fee: FEE_TIER,
-    amountIn: ethers.utils.parseEther('1'), // 1 WETH
-    sqrtPriceLimitX96: '0',
+    amountIn: ethers.parseEther(amountIn),
+    sqrtPriceLimitX96: '0'
   };
+  return await quoterContract.quoteExactInputSingle.staticCall(params);
+}
+
+async function getQuoteCbETHToETHExactIn(amountIn) {
+  const params = {
+    tokenIn: CBETH_ADDRESS,
+    tokenOut: WETH9_ADDRESS,
+    fee: FEE_TIER,
+    amountIn: ethers.parseEther(amountIn),
+    sqrtPriceLimitX96: '0'
+  };
+  return await quoterContract.quoteExactInputSingle.staticCall(params);
+}
+
+async function getQuoteCbETHToETHExactOut(amountOut) {
+  const params = {
+    tokenIn: CBETH_ADDRESS,
+    tokenOut: WETH9_ADDRESS,
+    fee: FEE_TIER,
+    amount: ethers.parseEther(amountOut),
+    sqrtPriceLimitX96: '0'
+  };
+  return await quoterContract.quoteExactOutputSingle.staticCall(params);
+}
+
+async function main() {
+  const ETHswapAmount = '0.0001';
+  const CbETHswapAmount = '0.00009211748539739';
+  const ETHAmountOut = '0.0002';
 
   try {
-    // Call the quoteExactInputSingle function for WETH -> cbETH
-    const quotedResultWethToCbeth =
-      await quoterContract.callStatic.quoteExactInputSingle(wethToCbethParams);
-
-    console.log('-'.repeat(30));
-    console.log(`1 WETH -> cbETH:`);
-
-    console.log(
-      `Quoted amount out (cbETH): ${ethers.utils.formatEther(
-        quotedResultWethToCbeth.amountOut
-      )} cbETH`
-    );
-    // console.log(`Sqrt Price After: ${quotedResultWethToCbeth.sqrtPriceX96After.toString()}`);
-    // console.log(`Initialized Ticks Crossed: ${quotedResultWethToCbeth.initializedTicksCrossed}`);
-    console.log(
-      `Gas Estimate: ${quotedResultWethToCbeth.gasEstimate.toString()} gas units`
-    );
     console.log('-'.repeat(30));
 
-    // Parameters for cbETH -> WETH swap
-    const cbethToWethParams = {
-      tokenIn: CBETH_ADDRESS,
-      tokenOut: WETH9_ADDRESS,
-      fee: FEE_TIER,
-      amountIn: ethers.utils.parseEther('1'), // Use the quoted cbETH amount from the first swap
-      sqrtPriceLimitX96: '0',
-    };
-
-    // Call the quoteExactInputSingle function for cbETH -> WETH
-    const quotedResultCbethToWeth =
-      await quoterContract.callStatic.quoteExactInputSingle(cbethToWethParams);
-
-    console.log(`1 cbETH -> WETH:`);
-
-    console.log(
-      `Quoted amount out (WETH): ${ethers.utils.formatEther(
-        quotedResultCbethToWeth.amountOut
-      )} WETH`
-    );
-    // console.log(`Sqrt Price After: ${quotedResultCbethToWeth.sqrtPriceX96After.toString()}`);
-    // console.log(`Initialized Ticks Crossed: ${quotedResultCbethToWeth.initializedTicksCrossed}`);
-    console.log(
-      `Gas Estimate: ${quotedResultCbethToWeth.gasEstimate.toString()} gas units`
-    );
+    // use below to estimate minimum cbETH to receive
+    const quoteETHToCbETHExactIn = await getQuoteETHToCbETHExactIn(ETHswapAmount);
+    console.log(`${ETHswapAmount} WETH -> cbETH Exact In: ${ethers.formatEther(quoteETHToCbETHExactIn.amountOut)} cbETH`);
     console.log('-'.repeat(30));
+
+    // const quoteCbETHToETHExactIn = await getQuoteCbETHToETHExactIn(CbETHswapAmount);
+    // console.log(`${CbETHswapAmount} cbETH -> WETH Exact In: ${ethers.formatEther(quoteCbETHToETHExactIn.amountOut)} WETH`);
+    // console.log('-'.repeat(30));
+
+    // use below to estimate cbETH to send back according to initial staked amount
+    const quoteCbETHToETHExactOut = await getQuoteCbETHToETHExactOut(ETHAmountOut);
+    console.log(`${ethers.formatEther(quoteCbETHToETHExactOut.amountIn)} cbETH -> WETH Exact Out: ${ETHAmountOut} WETH`);
+    console.log('-'.repeat(30));
+
   } catch (error) {
     console.error('Error fetching quote:', error);
   }
+
 }
 
 main().catch(error => {
