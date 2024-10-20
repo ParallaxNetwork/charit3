4d61ac031d821c38ef33d14901344e8ccb7a5f73
marcoelissa <marco.elissa@gmail.com>
2024-10-21 03:30:26 +0700

    Refactor VerifyPage component to use environment variables for API key and scorer ID

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from "next/navigation"
 import { getServerAuthSession } from "@/server/auth"
+import { env } from "@/env"
+
+const MINIMUM_SCORE = 2
 
 export default async function VerifyPage() {
   const session = await getServerAuthSession()
@@ -9,37 +12,43 @@ export default async function VerifyPage() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "X-API-KEY": "lWkfjrDP.ch7dekOp009z8E4Q5shyIfcfEnUP9OuD",
+        "X-API-KEY": env.GITCOIN_PASSPORT_API_KEY,
       },
       body: JSON.stringify({
         address: session?.user.address,
-        scorer_id: "8148",
+        scorer_id: env.GITCOIN_PASSPORT_SCORER_ID,
       }),
     },
   ).then((res) => res.json())
 
-  if (passport?.score > 2) {
+  if (passport?.score >= MINIMUM_SCORE) {
     return redirect("/home")
   }
 
   return (
-    <div className="container px-4 pb-24 pt-6">
-      <h1 className="text-center text-3xl font-semibold">Verify Humanity</h1>
-      <p className="mt-4 text-center">
-        Your score: {Number(passport?.score).toFixed(2)}
-      </p>
-      <p className="mt-4 text-center">
-        You need a minimum of 20 Proof of Humanity score to access Charit3.
-        Please go to{" "}
-        <a
-          href="https://passport.gitcoin.co/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gitcoin Passport
-        </a>
-        and verify your identity to increase your score.
-      </p>
+    <div className="container flex h-svh items-center px-4 py-6">
+      <div>
+        <h1 className="text-center text-3xl font-semibold">Verify Humanity</h1>
+        <p className="mt-4 text-center font-bold">
+          Your score: {Number(passport?.score).toFixed(2)}
+        </p>
+        <p className="mt-4 text-center">
+          You need a minimum of <b>{MINIMUM_SCORE}</b> Proof of Humanity score
+          to access Charit3.
+        </p>
+        <p className="mt-2 text-center">
+          Visit{" "}
+          <a
+            href="https://passport.gitcoin.co/"
+            className="text-blue underline hover:no-underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gitcoin Passport
+          </a>{" "}
+          to increase your score and then refresh this page.
+        </p>
+      </div>
     </div>
   )
 }
