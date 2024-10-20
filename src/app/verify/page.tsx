4a351190311cbbcac9cd5b1dd8e788f4ec0a00aa
marcoelissa <marco.elissa@gmail.com>
2024-10-21 02:43:17 +0700

    implement gitcoin passport

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,9 +1,45 @@
-import { redirect } from "next/navigation";
-import React from "react";
+import { redirect } from "next/navigation"
+import { getServerAuthSession } from "@/server/auth"
 
-const VerifyPage = () => {
-  redirect("/home"); // temp
-  return <div className="container pt-6 pb-24 px-4">VerifyPage</div>;
-};
+export default async function VerifyPage() {
+  const session = await getServerAuthSession()
+  const passport = await fetch(
+    "https://api.scorer.gitcoin.co/registry/submit-passport",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": "lWkfjrDP.ch7dekOp009z8E4Q5shyIfcfEnUP9OuD",
+      },
+      body: JSON.stringify({
+        address: session?.user.address,
+        scorer_id: "8148",
+      }),
+    },
+  ).then((res) => res.json())
 
-export default VerifyPage;
+  if (passport?.score > 2) {
+    return redirect("/home")
+  }
+
+  return (
+    <div className="container px-4 pb-24 pt-6">
+      <h1 className="text-center text-3xl font-semibold">Verify Humanity</h1>
+      <p className="mt-4 text-center">
+        Your score: {Number(passport?.score).toFixed(2)}
+      </p>
+      <p className="mt-4 text-center">
+        You need a minimum of 20 Proof of Humanity score to access Charit3.
+        Please go to{" "}
+        <a
+          href="https://passport.gitcoin.co/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gitcoin Passport
+        </a>
+        and verify your identity to increase your score.
+      </p>
+    </div>
+  )
+}
